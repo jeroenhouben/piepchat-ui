df7e0251ff8d48332da094c7b29d003e9f8ce593
Jeroen Houben <jeroen@supercool.nl>
2016-06-21 19:26:59 +0200

    JS VAlidations

---
+++ tests/unit/utils/user-validations-test.js
@@ -0,0 +1,10 @@
+import userValidations from 'peepchat/utils/user-validations';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | user validations');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = userValidations();
+  assert.ok(result);
+});
