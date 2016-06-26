d3338ed9251e96e3ce5124a5a071faf98f4de580
Jeroen Houben <jeroen@supercool.nl>
2016-06-26 14:26:29 +0200

    Rooms working

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,10 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import { hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
   email: attr('string'),
   password: attr('string'),
-  passwordConfirmation: attr('string')
+  passwordConfirmation: attr('string'),
+  rooms: hasMany('room')
 });
